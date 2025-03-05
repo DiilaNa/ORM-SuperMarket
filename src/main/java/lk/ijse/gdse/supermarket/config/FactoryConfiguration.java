96cc4b4d8edd3e9c6b565f6ca9582323bc5cd305
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-05 05:55:50 +0530

    hibernate added

---
+++ src/main/java/lk/ijse/gdse/supermarket/config/FactoryConfiguration.java
@@ -0,0 +1,5 @@
+package lk.ijse.gdse.supermarket.config;
+
+public class FactoryConfiguration {
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,13 @@ module lk.ijse.gdse.supermarket {
     requires mysql.connector.j;
     requires org.apache.commons.logging;
 
+    requires org.hibernate.orm.core;
+    requires jakarta.persistence;
+    requires java.naming;
+
+    opens lk.ijse.gdse.supermarket.entity to org.hibernate.orm.core;
+    opens lk.ijse.gdse.supermarket.config to jakarta.persistence;
+
     opens lk.ijse.gdse.supermarket.dto.tm to javafx.base;
     opens lk.ijse.gdse.supermarket.controller to javafx.fxml;
     exports lk.ijse.gdse.supermarket;
