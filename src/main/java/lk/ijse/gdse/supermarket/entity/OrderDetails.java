b3c85f65826810767034f4dcbdcbb65c3715c01b
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-05 06:13:08 +0530

    orderDetail entity updated

--- src/main/java/lk/ijse/gdse/supermarket/entity/OrderDetails.java
+++ src/main/java/lk/ijse/gdse/supermarket/entity/OrderDetails.java
@@ -1,15 +1,28 @@
 package lk.ijse.gdse.supermarket.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
-@Getter
-@Setter
+import java.util.Date;
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@Data
+@Entity
+@Table(name = "order_details")
+
 public class OrderDetails {
-    private String orderId;
-    private String itemId;
-    private int quantity;
-    private double price;
+    @Id
+    @Column(name = "order_id")
+    private String id;
+
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "cus_id")
+    private Customer customer;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderDetails> orderDetails;
 }
