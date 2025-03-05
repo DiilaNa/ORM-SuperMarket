655f877e1acae965011a7391eb00156289c732b5
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-05 05:58:39 +0530

    config added

--- src/main/java/lk/ijse/gdse/supermarket/config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/gdse/supermarket/config/FactoryConfiguration.java
@@ -1,5 +1,38 @@
 package lk.ijse.gdse.supermarket.config;
 
+
+import lk.ijse.gdse.supermarket.entity.Customer;
+import lk.ijse.gdse.supermarket.entity.Item;
+import lk.ijse.gdse.supermarket.entity.Order;
+import lk.ijse.gdse.supermarket.entity.OrderDetails;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private final SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        configuration.addAnnotatedClass(Customer.class)
+                .addAnnotatedClass(Item.class)
+                .addAnnotatedClass(Order.class)
+                .addAnnotatedClass(OrderDetails.class);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance() {
+        return (factoryConfiguration == null) ?
+                factoryConfiguration =
+                        new FactoryConfiguration() : factoryConfiguration;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
 
 }
+
