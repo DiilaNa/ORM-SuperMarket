39d2451c3e3916e3e574dd7dfb9c4eb66abc8cd0
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-05 06:07:19 +0530

    Item entity updated

--- src/main/java/lk/ijse/gdse/supermarket/entity/Item.java
+++ src/main/java/lk/ijse/gdse/supermarket/entity/Item.java
@@ -1,17 +1,31 @@
 package lk.ijse.gdse.supermarket.entity;
 
 
+import jakarta.persistence.*;
 import lombok.*;
 
-@Getter
-@Setter
+import java.math.BigDecimal;
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Entity
+@Table(name = "item")
 
 public class Item {
+    @Id
+    @Column(name = "item_id")
     private String itemId;
-    private String itemName;
+    @Column(length = 100)
+    private String name;
+
     private int quantity;
-    private double price;
+
+    @Column(name = "unit_price", precision = 10, scale = 2)
+    private BigDecimal unitPrice;
+
+    @OneToMany(mappedBy = "item")
+    private List<OrderDetails> orderDetails;
 }
 
