a1adb5a8a0e9c1145982a8bafac17201b8d1daf7
DiilaNa <liyanaarachchidilan@gmail.com>
2025-03-05 06:14:25 +0530

    orderDetailID entity updated

---
+++ src/main/java/lk/ijse/gdse/supermarket/entity/OrderDetailsId.java
@@ -0,0 +1,17 @@
+package lk.ijse.gdse.supermarket.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class OrderDetailsId {
+//    @Column(name = "order_id")
+    private String orderId;
+
+//    @Column(name = "item_id")
+    private String itemId;
+}
